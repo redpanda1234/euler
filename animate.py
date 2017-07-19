bfa479af9813842534f70d57b09630c8583e9c80
redpanda1234 <fkobayashi@g.hmc.edu>
2017-07-19 14:55:13 -0700

    working first anim

---
+++ animate.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
+from matplotlib import animation
+
+import euler
+
+N_trajectories = 20
+
+# Choose random starting points, uniformly distributed from -15 to 15
+np.random.seed(1)
+x0 = -15 + 30 * np.random.random((N_trajectories, 3))
+
+# Solve for the trajectories
+x_t = np.array([
+    euler.euler3D( 1000, .004, xi, euler.lorentz_deriv)
+    for xi in x0
+])
+
+# Set up figure & 3D axis for animation
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1], projection='3d')
+ax.axis('off')
+
+# choose a different color for each trajectory
+colors = plt.cm.jet(np.linspace(0, 1, N_trajectories))
+
+# set up lines and points
+lines = sum([ax.plot([], [], [], '-', c=c)
+             for c in colors], [])
+pts = sum([ax.plot([], [], [], 'o', c=c)
+           for c in colors], [])
+
+# prepare the axes limits
+ax.set_xlim((-25, 25))
+ax.set_ylim((-35, 35))
+ax.set_zlim((5, 55))
+
+# set point-of-view: specified by (altitude degrees, azimuth degrees)
+ax.view_init(30, 0)
+
+# initialization function: plot the background of each frame
+def init():
+    for line, pt in zip(lines, pts):
+        line.set_data([], [])
+        line.set_3d_properties([])
+
+        pt.set_data([], [])
+        pt.set_3d_properties([])
+    return lines + pts
+
+# animation function.  This will be called sequentially with the frame number
+def animate(i):
+    # reset the animation whenever it gets past the last frame
+    i = i % x_t.shape[1]
+
+    for line, pt, xi in zip(lines, pts, x_t):
+        x, y, z = xi[:i].T
+        line.set_data(x, y)
+        line.set_3d_properties(z)
+
+        pt.set_data(x[-1:], y[-1:])
+        pt.set_3d_properties(z[-1:])
+    ax.view_init(30, .3 * i)
+    fig.canvas.draw()
+    return lines + pts
+
+# instantiate the animator.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=500, interval=30, blit=True)
+
+plt.show()
+#     if writeout:
+#         mywriter = animation.FFMpegWriter(bitrate=4000)                                                                                # you have to install FFMpeg if you want to write out to mp4
+#         anim.save(str(N_trajectories)+'_trajectories_4x_n='+str(n)+'largersteps.mp4', writer='ffmpeg', fps=60, extra_args=['-vcodec', 'libx264']) # autogenerate filenames and export
+#         print("saved the file!")
+#         print("savetime is", time.time()-start_time)
+#     plt.close()
+#     print('combined runtime is', time.time() - first_time)
--- euler.py
+++ euler.py
@@ -16,11 +16,10 @@ def get_com(point_array):
 
 def lorentz_deriv(coord, sigma=10., beta=8./3, rho=28.0):
     """Compute the time-derivative of a Lorentz system."""
-    coord = [1,2,3]
     x, y, z = coord # unpack coordinates
     return np.array([sigma * (y - x), x * (rho - z) - y, x * y - beta * z])
 
-def euler(
+def euler2D(
         num_steps,
         step_size,
         initial_point=np.array([0.,0.]),
@@ -74,6 +73,60 @@ def euler(
         initial_point += np.array([step_size, step_size*derivative_function(initial_point[0])])
     return to_write
 
+def euler3D(
+        num_steps,
+        step_size,
+        initial_point=np.array([0.,0.,0.]),
+        derivative_function=lorentz_deriv
+    ):
+    """
+    Implements euler’s method for the input function.
+
+    Performs numerical integration over a finite amount of steps, by
+    approximating the function with the tangent line. Parameters below
+    described in the format <datatype, description>
+
+    ----------
+    Parameters
+    ----------
+    num_steps : int, describes the number of steps to take. For a
+                fixed step size, higher num_steps will increase the
+                amount of the function our approximation covers.
+
+    step_size : float, describes the size of each step.  Higher step
+                size will increase the amount of the function that the
+                approximation covers (for fixed num_steps), but at the
+                cost of accuracy.
+
+    initial_point : numpy array, describes the initial condition.
+                    Order is (x,y).  Defaults to np.array([0.,0.]).
+                    Datatype stored in the array should be float.
+
+    derivative_function : function, calculates the derivative of the
+                          mathematical function we're integrating.
+                          Should take inputs either of type int or type
+                          float. Defaults to derivative_x_squared,
+                          which returns 2*x.
+
+    -------
+    Returns
+    -------
+    out_array : numpy array, entries are other objects of type numpy
+                array.  Each entry should describe some point (x,y)
+                lying on our curve approximation.
+
+    ------------
+    Example call
+    ------------
+    point_array = euler(50, .25, initial_point=np.array([1.0,0.24]),
+                        derivative_function = math.cos)
+    """
+    to_write = np.zeros((num_steps, 3))
+    for n in range(num_steps):
+        to_write[n] = initial_point
+        initial_point += step_size*derivative_function(initial_point)
+    return to_write
+
 #vals = euler(20,.25, initial_point=np.array([0.,0.]))
 #print(vals)
 #x,y = [val[0] for val in vals], [val[1] for val in vals]
@@ -83,9 +136,9 @@ def euler(
 #plt.plot(x,y, "o")
 #plt.show()
 
-x = np.linspace(0,2*np.pi, 50) # this is similar to range()
-y = np.cos(x)
-y2 = y**2
-plt.plot(x,y,"bo") # creates the plot with blue circle markers
-plt.plot(x,y2,"g^") # same but green triangles pointing upward
-plt.show()
+# x = np.linspace(0,2*np.pi, 50) # this is similar to range()
+# y = np.cos(x)
+# y2 = y**2
+# plt.plot(x,y,"bo") # creates the plot with blue circle markers
+# plt.plot(x,y2,"g^") # same but green triangles pointing upward
+# plt.show()
